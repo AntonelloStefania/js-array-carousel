02a123d8c93a1149ce060bd4d466aabda9724935
AntonelloStefania <antonelloste94@gmail.com>
2023-05-23 17:28:59 +0200

    [add] loop immagini, bonus 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//array immagini
 let imgs = [
     "01.webp",
     "02.webp",
@@ -10,6 +11,7 @@ let imgs = [
 let img_container = document.querySelector('.img-container');
 let slide ="";
 
+//ciclo for per le immagini
 for(let i=0; i < imgs.length; i++){
     let current_img = `<img src="./img/${imgs[i]}" alt="">`
     slide += `<div class="slide">
@@ -22,22 +24,26 @@ let active_slide = 0;
 let all_slides = document.getElementsByClassName('slide')
 all_slides[active_slide].classList.add('active');
 
+//variabili associazione pulsanti
 let prev_btn = document.querySelector("#prev-button");
 let next_btn = document.querySelector("#next-button");
 
+//pulsante per andare avanti
 next_btn.addEventListener('click', function(){
-    if(active_slide < (imgs.length - 1)){
+    
         all_slides[active_slide].classList.remove('active');
-        active_slide++;
+        active_slide = (active_slide + 1) % imgs.length ;
         all_slides[active_slide].classList.add('active');
  
-    } 
+   
 })
 
+//Pulsante per tornare indietro
+
 prev_btn.addEventListener('click', function(){
-    if(active_slide > 0){
+    
         all_slides[active_slide].classList.remove('active');
-        active_slide--;
+        active_slide=(active_slide -1 + imgs.length) % imgs.length;
         all_slides[active_slide].classList.add('active');
-    }
+    
 })
